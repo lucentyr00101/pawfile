b14a1f814d55b2cafd4c7a34c64d8f2efde58691
Ranz Sarcos <ranzsarcos@MacBook-Air.local>
2026-04-11 22:49:59 +0800

    feat: install and configure nuxt-auth-utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image', 'nuxt-auth-utils'],
   css: ['~/assets/css/main.css'],
   ui: {
     theme: {
