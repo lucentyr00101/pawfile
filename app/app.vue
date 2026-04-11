cca63672fa737afd59bf1375331f520b4fa5eb23
Ranz Sarcos <ranzsarcos@MacBook-Air.local>
2026-04-11 22:40:12 +0800

    feat: add design system and Nuxt UI color configuration

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <UButton color="primary">Primary</UButton>
+    <UButton color="secondary">Secondary</UButton>
+    <UButton color="tertiary">Tertiary</UButton>
+    <UButton color="neutral">Neutral</UButton>
+  </UApp>
 </template>
